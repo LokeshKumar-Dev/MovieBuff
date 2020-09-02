ca3d53a7ef3022d95b759b9ad086e9aa8f740023
lokesh <adobep40@gmail.com>
2020-09-02 00:36:29 -0700

    Updates

---
+++ precache-manifest.809ec9db25a392a68747449f926496df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2d63e9e55c99ce24797e8bdd107f58",
+    "url": "/MovieBuff/index.html"
+  },
+  {
+    "revision": "6fe605063702c709bccf",
+    "url": "/MovieBuff/static/css/main.17a4c955.chunk.css"
+  },
+  {
+    "revision": "3a53ca1f4444d711a442",
+    "url": "/MovieBuff/static/js/2.edc5bb94.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MovieBuff/static/js/2.edc5bb94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe605063702c709bccf",
+    "url": "/MovieBuff/static/js/main.6138d7d9.chunk.js"
+  },
+  {
+    "revision": "0acba13114fb8a28588a",
+    "url": "/MovieBuff/static/js/runtime-main.e7c7f46d.js"
+  }
+]);
